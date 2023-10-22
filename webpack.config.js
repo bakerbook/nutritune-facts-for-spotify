8c5e04f4fce599dc6ca3268026bbb16f154bd6ae
baker-1234 <bakerjbook@gmail.com>
2023-10-22 00:51:00 -0700

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+import path from "path"
+
+const __dirname = path.resolve('')
+
+export default {
+    entry: './src/main.tsx',
+
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    }
+}
