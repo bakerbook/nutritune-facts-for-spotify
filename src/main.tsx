40187b6af8d6f5c1d5d44e42d62cec3eac362cf3
baker-1234 <bakerjbook@gmail.com>
2023-12-29 12:48:11 -0800

    Added a components folder within src and put existing components inside it

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import Header from './Header'
-import LoginButton from './LoginButton';
+import ReactDOM from 'react-dom/client'
 import './styles.css'
+import Header from './components/Header'
+import LoginButton from './components/LoginButton'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
