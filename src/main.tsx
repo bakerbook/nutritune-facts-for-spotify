a8c417307ab0f7a4bb3e7a7b0a4329b932ed1e50
baker-1234 <bakerjbook@gmail.com>
2024-01-14 23:46:52 -0800

    Cleaned some things up, temporarily removed dark mode, changed playlist cover rounding to align with Spotify's TOS

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import './styles.css'
 import Header from './components/Header'
 import LoginComponent from './components/LoginComponent'
-import SelectPlaylist from './components/SelectPlaylist'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
