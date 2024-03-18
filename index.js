85961067f5de6cf1a8b090bbeff1d0417f6f211d
baker-1234 <bakerjbook@gmail.com>
2024-03-17 19:07:46 -0700

    Fix an issue with Heroku port

--- index.js
+++ index.js
@@ -118,8 +118,8 @@ app.post("/api/getPlaylistDetails", async (req, res) => {
     res.send(JSON.stringify(data))
 })
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`)
+app.listen(process.env.PORT || port, () => {
+    console.log(`App listening on port ${process.env.PORT || port}`)
 })
 
 async function getProfileInformation(accessToken){
